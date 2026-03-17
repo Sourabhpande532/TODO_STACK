1e5197aafac95e4d90c05f61038e0c0f4bbc1909
sourabhpande532 <sourabhpande43@gmail.com>
2026-03-17 21:25:13 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to OAuth API Server</h1>`);
+});
+console.log(process.env.CLIENT_ID);
+
+app.get("/auth/github", (req, res) => {
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`;
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
