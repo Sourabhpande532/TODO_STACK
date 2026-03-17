4731547c60b68f371530ede25b09a3b085a964d5
sourabhpande532 <sourabhpande43@gmail.com>
2026-03-17 21:57:20 +0530

    Fix: server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.get("/", (req, res) => {
   res.send(`<h1>Welcome to OAuth API Server</h1>`);
 });
 console.log(process.env.CLIENT_ID);
+console.log(process.env.CLIENT_SECRET);
 
 app.get("/auth/github", (req, res) => {
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`;
